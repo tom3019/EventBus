35f56c2ef205ccdf150493f60b60b2d26715e8c9
Tom Chang <tom90440@gmail.com>
2024-05-04 20:27:35 +0800

    add IEventHandlerInvoker

---
+++ EventBus/IEventHandlerInvoker.cs
@@ -0,0 +1,16 @@
+﻿namespace EventBus;
+
+/// <summary>
+///   Event Handler Invoker Interface
+/// </summary>
+public interface IEventHandlerInvoker
+{
+    /// <summary>
+    ///    Invokes the specified event handlers that are registered to handle the given event asynchronously.
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="cancellationToken"></param>
+    /// <typeparam name="TEvent"></typeparam>
+    /// <returns></returns>
+    Task InvokeAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default);
+}
